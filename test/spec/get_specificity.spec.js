bacca0925001ec7b205c581688cc87b790e2b8e4
Adonis K <aklp08@gmail.com>
2015-11-21 15:51:32 +0200

    add more expectations in get specificity tests

--- test/spec/get_specificity.spec.js
+++ test/spec/get_specificity.spec.js
@@ -1,9 +1,25 @@
 describe('getSpecificity', function () {
     'use strict';
 
-    it('should return 0 if the arguments are not valid', function () {
+    it('should return 0 if the arguments contain no valid data', function () {
         expect(uniquePath.getSpecificity()).toBe(0);
         expect(uniquePath.getSpecificity({})).toBe(0);
+        expect(uniquePath.getSpecificity([])).toBe(0);
+    });
+
+    it('should return 1 if the given path is targetting body', function () {
+        var path = [
+            {
+                node: {},
+                separator: ' ',
+                selector: {
+                    type: 'name',
+                    value: 'body'
+                }
+            }
+        ];
+
+        expect(uniquePath.getSpecificity(path)).toBe(1);
     });
 
     // TODO: extract path arrays and evaluate them here
